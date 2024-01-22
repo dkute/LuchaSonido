584835aa9e1a4bfe861ee1966d1ca82077f0a62c
Sarayminar <pss00098@educastur.es>
2024-01-22 12:27:14 +0100

    Vuelta la cordura

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-//hola
+//hola mundo
 public class ChangeScene : MonoBehaviour
 {
     public string sceneToLoad;
