a0d2b9e3b6458fc26f259b3db38103f6f61b44dc
dkute <nathalymm05@educastur.es>
2024-01-23 11:44:30 +0100

    personaje se mueve y animacion idle+walking

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +5,48 @@ using UnityEngine;
 public class Movimiento : MonoBehaviour
 {
     Rigidbody2D rb2d;
+    SpriteRenderer spRd;
     private Animator animator;
-   
-    public float velocidadMovimiento = 5f;
-  
 
+    public float velMov = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
     void Update()
     {
         float movimientoH = Input.GetAxis("Horizontal");
-        Vector2 mov = new Vector2(movimientoH, 0);
-        GetComponent<Rigidbody2D>().velocity = mov * velocidadMovimiento;
-       
+        Vector2 movi = new Vector2(movimientoH, 0);
+        GetComponent<Rigidbody2D>().velocity = movi * velMov;
+        walk(movimientoH);
     }
 
-   
+    private void GiroHorizontal(float movimientoH)
+    {
+        if (movimientoH > 0)
+        {
+            spRd.flipX = false;
+        }
+        else if (movimientoH < 0)
+        {
+            spRd.flipY = true;
+        }
     }
+    private void walk(float movimientoH)
+    {
+        if (movimientoH != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
 
-    
+
+
