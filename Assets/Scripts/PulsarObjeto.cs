5892af70c67ac0d652c59d0664a46352c73dc94a
Manulavi <HQA70577@educastur.es>
2024-02-05 13:29:45 +0100

    problema canvas

--- Assets/Scripts/PulsarObjeto.cs
+++ Assets/Scripts/PulsarObjeto.cs
@@ -7,19 +7,34 @@ public class PulsarObjeto : MonoBehaviour
 {
     public string sceneName;
     public Canvas canvasSuperposicion;
+    public Canvas canvasSuperposicion2;
 
-    public void OnMouseDown ()
+
+    public void Anuncios ()
     {
-       
+        canvasSuperposicion.gameObject.SetActive(true);
+        
+    }
+    public void BotonPlay()//Permite cambiar de escena
+    {
         SceneManager.LoadScene(sceneName);
     }
-    private void OnMouseOver()
+    public void OnMouseOver() //Permite Superponer dentro de la misma escena
     {
         canvasSuperposicion.gameObject.SetActive(true);
     }
-    private void OnMouseExit()
+    public void OnMouseExit()
     {
         canvasSuperposicion.gameObject.SetActive(false);
+    }
+
+    public void ajustes() //Permite Superponer dentro de la misma escena
+    {
+        canvasSuperposicion.gameObject.SetActive(true);
+    }
+    public void Inventario() //Permite Superponer dentro de la misma escena
+    {
+        canvasSuperposicion2.gameObject.SetActive(true);
     }
 }
 
