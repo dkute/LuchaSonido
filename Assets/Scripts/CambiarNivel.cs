99596f9d623f4ecaf5360054dd9fdc9826ad0d00
Manulavi <HQA70577@educastur.es>
2024-02-22 12:37:26 +0100

    premerge

--- Assets/Scripts/CambiarNivel.cs
+++ Assets/Scripts/CambiarNivel.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class CambiarNivel : MonoBehaviour
 {
     public int sceneBuildIndex;
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
     {
         print("Trigger Entered");
-        if (collision.tag == "Player")
+        if (other.tag == "Player")
         {
-            print("Switching Scene to " + sceneBuildIndex);
+            print("Switching Scene to" + sceneBuildIndex);
             SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
         }
     }
-   
 }
