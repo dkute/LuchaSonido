f3d41f83d3e6cf4d9fa91fb58e8994939a10bdfd
dkute <nathalymm05@educastur.es>
2024-02-25 19:15:16 +0100

    pause
    funciona pero el mensaje de superposicion siempre es correcto

--- Assets/Scripts/MiniJuegoSimonDice/SimonDice.cs
+++ Assets/Scripts/MiniJuegoSimonDice/SimonDice.cs
@@ -1,22 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-//hola saray
+
 public class SimonDice : MonoBehaviour
 {
-    public AudioClip[] notas; // Array que contiene los sonidos de las notas
-    private List<int> secuenciaNotas = new List<int>(); // Lista para almacenar la secuencia de notas generada aleatoriamente
-    private int indexSecuencia = 0; // Índice para rastrear la posición actual en la secuencia de notas
-    private bool jugadorTurno = false; // Variable para rastrear si es el turno del jugador
+    public AudioClip[] notas;
+    private List<int> secuenciaNotas = new List<int>();
+    private int indexSecuencia = 0;
+    private bool jugadorTurno = false;
     private List<int> secuenciaJugador = new List<int>();
-    public  GameObject  canvasSuperposicion;
+    public GameObject canvasInicio;
+    public GameObject canvasSuperposicion;
     public GameObject canvasVictoria;
     public GameObject canvasDerrota;
+    public TMP_Text puntuacionText;
     public TMP_Text aciertosText;
     public TMP_Text fallosText;
-
     public Button A;
     public Button B;
     public Button C;
@@ -25,91 +26,86 @@ public class SimonDice : MonoBehaviour
     public Button F;
     public Button G;
 
-    private int notasResponder = 1; //numero de notas que el jugador debe responder
-    private int notasRespondidas = 0; //notas que el jugador ha respondido
-    private int intentos = 0; //numero de intentos/aciertos? del jugador
-    private int fallos = 0; //numero de fallos
+    private int notasResponder = 1;
+    private int notasRespondidas = 0;
+    private int intentos = 0;
+    private int fallos = 0;
+
     void Start()
     {
         canvasSuperposicion.SetActive(false);
-        // Inicia el juego
-        //StartCoroutine(IniciarSecuencia());
-        A.onClick.AddListener(() => NotaPresionada(0));
-        A.onClick.AddListener(() => NotaPresionada(1));
-        B.onClick.AddListener(() => NotaPresionada(2));
-        C.onClick.AddListener(() => NotaPresionada(3));
-        D.onClick.AddListener(() => NotaPresionada(4));
-        E.onClick.AddListener(() => NotaPresionada(5));
-        F.onClick.AddListener(() => NotaPresionada(6));
-        G.onClick.AddListener(() => NotaPresionada(7));
-        StartCoroutine(IniciarSecuencia());
+        canvasVictoria.SetActive(false);
+        canvasDerrota.SetActive(false);
+        canvasInicio.SetActive(true);
 
+        A.onClick.AddListener(() => NotaPresionada(0));
+        B.onClick.AddListener(() => NotaPresionada(1));
+        C.onClick.AddListener(() => NotaPresionada(2));
+        D.onClick.AddListener(() => NotaPresionada(3));
+        E.onClick.AddListener(() => NotaPresionada(4));
+        F.onClick.AddListener(() => NotaPresionada(5));
+        G.onClick.AddListener(() => NotaPresionada(6));
     }
 
-    IEnumerator IniciarSecuencia()
+    public void IniciarJuego()
     {
-        yield return new WaitForSeconds(1f); // Retardo inicial antes de iniciar la secuencia
-        secuenciaNotas.Add(Random.Range(0, notas.Length));
-        while (intentos < 3 && fallos < 3) //limita el  juego a 3 intentos
+        canvasInicio.SetActive(false);
+        StartCoroutine(RealizarJuego());
+    }
+
+    IEnumerator RealizarJuego()
+    {
+        Debug.Log("Iniciando juego...");
+
+        ReproducirNota(Random.Range(0, notas.Length));
+        yield return new WaitForSeconds(1f);
+
+        while (intentos < 3 && fallos < 3)
         {
+            Debug.Log("Nuevo turno...");
+
             canvasSuperposicion.SetActive(false);
             secuenciaJugador.Clear();
-            notasRespondidas = 0; //establece el numero de notas que el jugador debe responder
+            notasRespondidas = 0;
             notasResponder = secuenciaNotas.Count + 1;
 
-            // Agrega una nueva nota aleatoria a la secuencia
-            //int nuevaNota = Random.Range(0, notas.Length);
-            //secuenciaNotas.Add(nuevaNota);
-
-            //condicional para que sepa cuantas notas etc
-
-            // Reproduce la secuencia actual de notas
             yield return ReproducirSecuencia();
 
-            //jugadorTurno = true; // Es el turno del jugador
-            //yield return new WaitUntil(1f); // Retardo antes de que el jugador pueda comenzar a replicar la secuencia
-            //yield return new WaitUntil(() => indexSecuencia >= secuenciaNotas.Count); // Espera a que el jugador complete la secuencia
             jugadorTurno = true;
             yield return new WaitUntil(() => notasRespondidas >= notasResponder);
 
-            
-
             if (!ComprobarSecuencia())
             {
-                fallos++; //incrementa el contador de fallos 
-                Debug.Log("Has cometido un error Repitiendo secuencia!...");
-                //secuenciaNotas.Clear();
-                canvasSuperposicion.SetActive(true);
-                //indexSecuencia = 0;
-                
-                yield return new WaitForSeconds(1f);
+                fallos++;
+                fallosText.text = "Fallos: " + fallos.ToString();
+                puntuacionText.text = "Vuelve a intentar, eres un incompetente";
+                MostrarMensajeSuperposicion("Vuelve a intentar");
+                Debug.Log("Error en la secuencia. Fallos: " + fallos);
+                yield return new WaitForSeconds(1.5f);
             }
             else
             {
-                // Avanza al siguiente nivel si el jugador ha respondido correctamente todas las notas
                 indexSecuencia = 0;
                 secuenciaNotas.Add(Random.Range(0, notas.Length));
                 yield return new WaitForSeconds(1f);
             }
 
-             //Reinicia el índice para la siguiente ronda
-            //indexSecuencia = 0;
-
-            jugadorTurno = false; // La secuencia se está reproduciendo nuevamente
+            jugadorTurno = false;
+            canvasSuperposicion.SetActive(true);
+            Debug.Log("Fin del turno del jugador. Mostrando mensaje de superposición.");
+            yield return new WaitForSeconds(1.5f);
+            canvasSuperposicion.SetActive(false);
         }
-        // Comprueba si el jugador ha ganado o perdido después de 3 intentos
+
         if (intentos >= 3 || fallos >= 3)
         {
-            canvasSuperposicion.SetActive(false);
             canvasDerrota.SetActive(true);
-            puntuacionText.text = "Tu puntuación: " + (secuenciaNotas.Count - fallos);
             Debug.Log("Juego terminado. Has alcanzado el límite de intentos. ¡Intenta de nuevo!");
         }
         else
         {
-            canvasSuperposicion.SetActive(false);
             canvasVictoria.SetActive(true);
-            puntuacionText.text = "Tu puntuación: " + secuenciaNotas.Count;
+            puntuacionText.text = "Tu puntuación: " + secuenciaNotas.Count.ToString();
             Debug.Log("¡Has ganado! Completaste la secuencia.");
         }
     }
@@ -118,74 +114,69 @@ public class SimonDice : MonoBehaviour
     {
         foreach (int nota in secuenciaNotas)
         {
-            
-            ReproducirNota(nota); //repr
-            yield return new WaitForSeconds(1.5f); // Retardo entre notas
+            yield return new WaitForSeconds(0.5f);
+            ReproducirNota(nota);
         }
     }
-  
 
     void ReproducirNota(int indice)
     {
-        // Reproduce el sonido de la nota en el índice especificado
         AudioSource.PlayClipAtPoint(notas[indice], transform.position);
     }
 
-   
     bool ComprobarSecuencia()
     {
         for (int i = 0; i < secuenciaNotas.Count; i++)
         {
             if (secuenciaNotas[i] != secuenciaJugador[i])
             {
-                fallosText.text = "Fallos " + fallos.ToString();
-                //canvasSuperposicion.SetActive(true);
-               return false;
+                fallosText.text = "Fallos: " + fallos.ToString();
+                puntuacionText.text = "Eres un incompetente, vuelve a intentar";
+                MostrarMensajeSuperposicion("Vuelve a intentar");
+                Debug.Log("Error en la secuencia. Fallos: " + fallos);
+                return false;
             }
+
         }
+
         aciertosText.text = "Aciertos: " + secuenciaNotas.Count.ToString();
-       return true;
-        
+        MostrarMensajeSuperposicion("Correcto");
+        Debug.Log("Secuencia correcta. Aciertos: " + secuenciaNotas.Count);
+        return true;
     }
 
     public void NotaPresionada(int indice)
     {
         if (jugadorTurno)
         {
-           // ReproducirNota(indide);
+            ReproducirNota(indice);
             secuenciaJugador.Add(indice);
             notasRespondidas++;
-            // Comprueba si la nota presionada por el jugador coincide con la siguiente nota en la secuencia
-            //if (indice == secuenciaNotas[indexSecuencia])
+
             if (notasRespondidas >= notasResponder)
             {
-                //indexSecuencia++; // Avanza al siguiente índice en la secuencia
-                //if(indexSecuencia >= secuenciaNotas.Count)
                 if (!ComprobarSecuencia())
                 {
-                    canvasSuperposicion.SetActive(true);
-                    Debug.Log("¡Has perdido! Intenta de nuevo.");
-                    fallos++; //incrementa el contador de fallos
-                    fallosText.text = "Fallos: " + fallos.ToString(); //actualiza el texto
+                    fallos++;
+                    fallosText.text = "Fallos: " + fallos.ToString();
                     secuenciaNotas.Clear();
                     StartCoroutine(ReproducirSecuencia());
                 }
-                
-                    //StartCoroutine(IniciarSecuencia());
-                
             }
-            
-            //else
-            {
-               
-                //canvasSuperposicion.SetActive(true);
-                // El jugador cometió un error, puedes agregar aquí la lógica para manejarlo
-               // Debug.Log("¡Has perdido! Intenta de nuevo.");
-                //secuenciaNotas.Clear(); // Reinicia la secuencia para comenzar de nuevo
-                //StartCoroutine(ReproducirSecuencia()); // Reinicia el juego //mirarlo
-            }
-            
+        }
+    }
+
+    void MostrarMensajeSuperposicion(string mensaje)
+    {
+        TMP_Text mensajeText = canvasSuperposicion.GetComponentInChildren<TMP_Text>();
+
+        if (mensajeText != null)
+        {
+            mensajeText.text = mensaje;
+        }
+        else
+        {
+            Debug.LogError("No se encontró un componente TextMeshProUGUI en el canvas de superposición.");
         }
     }
 }
-
