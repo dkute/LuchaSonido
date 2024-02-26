f8bb5ea69daf1dd8ff5e368978e436a9891fc03b
dkute <nathalymm05@educastur.es>
2024-02-26 14:20:51 +0100

    Cositas manu
    inicio, mp4, inventario, ajsutes, salidas, energia

--- Assets/Scripts/PulsarObjeto.cs
+++ Assets/Scripts/PulsarObjeto.cs
@@ -6,11 +6,40 @@ using UnityEngine;
 public class PulsarObjeto : MonoBehaviour
 {
     public string sceneName;
+    public string sceneName2;
     public Canvas canvasSuperposicion;
+    public Canvas canvasSuperposicion2;
+    public Canvas canvasSuperposicion3;
 
-    private void OnMouseDown()
+    public void cambiarScene()
     {
         SceneManager.LoadScene(sceneName);
+        
+    }
+    public void cambiarScene2()
+    {
+        SceneManager.LoadScene(sceneName2);
+
+    }
+
+    public void canvasEnergia()
+    {
+        canvasSuperposicion.gameObject.SetActive(true);
+    }
+    public void canvasVolver()
+    {
+        canvasSuperposicion.gameObject.SetActive(false);
+        canvasSuperposicion2.gameObject.SetActive(false);
+        canvasSuperposicion3.gameObject.SetActive(false);
+    }
+
+    public void canvasInventario()
+    {
+        canvasSuperposicion2.gameObject.SetActive(true);
+    }
+    public void canvasAjustes()
+    {
+        canvasSuperposicion3.gameObject.SetActive(true);
     }
     private void OnMouseOver()
     {
